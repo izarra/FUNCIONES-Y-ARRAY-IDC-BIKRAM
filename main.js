0ba858be92abd4259036452a7f8f7eb587990bad
IDCIzarraDomingoCansino <31973220+izarra@users.noreply.github.com>
2023-06-01 02:46:12 +0200

    Add files via upload
    Falta por resolver unos cuantos

---
+++ main.js
@@ -0,0 +1,86 @@
+//*1
+cons arrayVacio=[];
+
+//*2
+const arrayNumeros= [0,1,2,3,4,5,6,7,8,9];
+
+//*3
+const arraNumerosPares=[0,2,4,6,8];
+
+//*4
+const arrayBidimensional= [[0,1,2],['a','b','c']];
+
+//*5
+const suma= (a,b)=>{
+   return a+b;
+};
+
+//*6
+const potenciacion=(a,b)=>{
+    return a**b;
+};
+
+//*7
+const separarPalabras= (String)=>{
+    return String.split("");
+};
+
+//*8
+function repetir String(String,num)=>{
+    return String.repeat(num);
+}
+
+//*9
+function esPrimo(numero)=>{
+    for (let index=2;index<numero;index++){
+        if (numero%index===0){
+            return false
+        }
+    }
+    return true
+}
+
+//*10
+
+//*11
+
+//*12
+
+//*13
+
+//*14
+
+//*15
+
+//*16
+
+//*17
+
+//*18
+
+//*19
+
+//*20
+
+//*21
+
+//*22
+
+//*23
+
+//*24
+
+//*25
+
+//*26
+
+
+
+
+
+
+
+
+
+
+
